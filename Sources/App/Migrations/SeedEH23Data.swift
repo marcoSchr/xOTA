c39ab4f887024dc77ff11b3a605ff38f2f9467d6
marco <49691247+marcoSchr@users.noreply.github.com>
2026-04-03 15:13:19 +0200

    Seed EH23 Toilets

--- Sources/App/Migrations/SeedEH23Data.swift
+++ Sources/App/Migrations/SeedEH23Data.swift
@@ -6,10 +6,16 @@ struct SeedEH23Data: AsyncMigration {
 		let references = [
 			Reference(title: "T-01", website: ""),
 			Reference(title: "T-02", website: ""),
+			Reference(title: "T-03", website: ""),
 			Reference(title: "T-11", website: ""),
+			Reference(title: "T-13", website: ""),
+			Reference(title: "T-14", website: ""),
 			Reference(title: "T-21", website: ""),
 			Reference(title: "T-31", website: ""),
+			Reference(title: "T-33", website: ""),
+			Reference(title: "T-33", website: ""),
 			Reference(title: "T-41", website: ""),
+			Reference(title: "T-51", website: ""),
 		]
 
 		for reference in references {
